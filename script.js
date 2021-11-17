e2197d4c1e773c6927c42743f9ba6a6cbd8517aa
Eduardo Neves <edu.5.n.silva@gmail.com>
2021-11-17 10:56:37 -0300

    Inserindo todas as informações das pizzas

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const cs = el => document.querySelectorAll(el)
 pizzaJson.map((item, index) => {
     let pizzaItem = c('.models .pizza-item').cloneNode(true)
 
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
     pizzaItem.querySelector('.pizza-item--name').append(item.name)
-    console.log(item.name)
+    pizzaItem.querySelector('.pizza-item--desc').append(item.description)
+    pizzaItem.querySelector('.pizza-item--price').append(`R$ ${item.price.toFixed(2)}`)
 
     c('.pizza-area').append(pizzaItem)
 })
