ca896337561f3aa09f646fc9258cb19ced8bf8df
Eduardo Neves <edu.5.n.silva@gmail.com>
2021-11-20 16:34:37 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -100,15 +100,33 @@ c('.pizzaInfo--addButton').addEventListener('click', e => {
     closeModal()
 })
 
-const updateCart = _ => {
+c('.menu-openner').addEventListener('click', _=>{
+    if(cart.length > 0){
+        c('aside').style.left = '0'
+    }
+})
+
+c('.menu-closer').addEventListener('click', _=> {
+    c('aside').style.left = '100vw'
+})
+
+function updateCart() {
+    c('.menu-openner span').innerHTML = cart.length
+
     if(cart.length > 0){
         c('aside').classList.add("show")
         c('.cart').innerHTML = ''
+
+        let subtotal = 0
+        let desconto = 0
+        let total = 0
 
 
         for(let i in cart){
             let pizzaItem = pizzaJson.find(item => item.id == cart[i].id)
             let cartItem = c('.models .cart--item').cloneNode(true)
+
+            subtotal += pizzaItem.price * cart[i].qt
 
             let pizzaSize = ''
             switch (cart[i].size) {
@@ -130,9 +148,33 @@ const updateCart = _ => {
             cartItem.querySelector('.cart--item-nome').innerHTML = `${pizzaItem.name} (${pizzaSize})`
             cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt
 
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', _ => {
+                if(cart[i].qt > 1){
+                    cart[i].qt--;                    
+                }else{
+                    cart.splice(i, 1)
+                }
+
+                updateCart()
+            
+            })
+            
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', _ => {
+                cart[i].qt++;
+                updateCart()
+            })
+
             c('.cart').append(cartItem)
+
+            desconto = subtotal * 0.1
+            total = subtotal - desconto
+
+            c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`
+            c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`
+            c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`
         }
     }else{
         c('aside').classList.remove("show")
+        c('aside').style.left = '100vw'
     }
 }
